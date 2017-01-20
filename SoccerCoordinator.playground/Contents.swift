bfc19a46e770a8990c5c35dc9fcd4b3713d00832
Jparks <boatcoding@gmail.com>
2017-01-20 15:02:25 -0800

    Player data..

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,25 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+// a variable that holds all the players in the league
 
-// Josh's setup test comment
+var jSmith: [String: String] = ["Name": "Joe Smith", "Height": "42", "Experience": "Yes", "Guardian": "Jim and Jan Smith"]
+var jTanner: [String: String] = ["Name": "Jill Tanner", "Height": "36", "Experience": "Yes", "Guardian": "Clara Tanner"]
+var bBon: [String: String] = ["Name": "Bill Bonn", "Height": "43", "Experience": "Yes", "Guardian": "Sara and Jenny Bon"]
+var eGordon: [String: String] = ["Name": "Eva Gordon", "Height": "45", "Experience": "No", "Guardian": "Wendy and Mike Gordon"]
+var mGill: [String: String] = ["Name": "Matt Gill", "Height": "40", "Experience": "No", "Guardian": "Charles and Sylvia Gill"]
+var kStein: [String: String] = ["Name": "Kimmy Stein", "Height": "41", "Experience": "No", "Guardian": "Bill and Hillary Stein"]
+var sAdams: [String: String] = ["Name": "Sammy Adams", "Height": "45", "Experience": "No", "Guardian": "Jeff Adams"]
+var kSaygan: [String: String] = ["Name": "Karl Saygan", "Height": "42", "Experience": "Yes", "Guardian": "Heather Bledsoe"]
+var sGreenberg: [String: String] = ["Name": "Suzane Greenberg", "Height": "44", "Experience": "Yes", "Guardian": "Henrietta Dumas"]
+var sDali: [String: String] = ["Name": "Sal Dali", "Height": "41", "Experience": "No", "Guardian": "Gala Dali"]
+var jKavalier: [String: String] = ["Name": "Joe Kavalier", "Height": "39", "Experience": "No", "Guardian": "Sam and Elaine Kavalier"]
+var bFinkelstein: [String: String] = ["Name": "Ben Finkelstein", "Height": "44", "Experience": "No", "Guardian": "Aaron and Jill Finkelstein"]
+var dSoto: [String: String] = ["Name": "Diego Soto", "Height": "41", "Experience": "Yes", "Guardian": "Robin and Sarika Soto"]
+var cAlaska: [String: String] = ["Name": "Chloe Alaska", "Height": "47", "Experience": "No", "Guardian": "David and Jamie Alaska"]
+var aWillis: [String: String] = ["Name": "Arnold Willis", "Height": "43", "Experience": "No", "Guardian": "Claire Willis"]
+var pHelm: [String: String] = ["Name": "Phillip Helm", "Height": "44", "Experience": "Yes", "Guardian": "Thomas Helm and Eva Jones"]
+var lClay: [String: String] = ["Name": "Les Clay", "Height": "42", "Experience": "Yes", "Guardian": "Wynonna Brown"]
+var hKrustofski: [String: String] = ["Name": "Herschel Krustofski", "Height": "45", "Experience": "Yes", "Guardian": "Hyman and Rachel Krustofski"]
+
+
+
