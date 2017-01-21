d02485d4883bb134c1e1989b8689da9e5d6c6dd1
Jparks <boatcoding@gmail.com>
2017-01-20 16:06:57 -0800

    Part 2

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -20,6 +20,8 @@ var aWillis: [String: String] = ["Name": "Arnold Willis", "Height": "43", "Exper
 var pHelm: [String: String] = ["Name": "Phillip Helm", "Height": "44", "Experience": "Yes", "Guardian": "Thomas Helm and Eva Jones"]
 var lClay: [String: String] = ["Name": "Les Clay", "Height": "42", "Experience": "Yes", "Guardian": "Wynonna Brown"]
 var hKrustofski: [String: String] = ["Name": "Herschel Krustofski", "Height": "45", "Experience": "Yes", "Guardian": "Hyman and Rachel Krustofski"]
+pHelm
+pHelm["Experience"]
 
 // all players within the league are now stored within this one array
 var players = [jSmith, jTanner, bBon, eGordon, mGill, kStein, sAdams, kSaygan, sGreenberg, sDali, jKavalier, bFinkelstein, dSoto, cAlaska, aWillis, pHelm, lClay, hKrustofski]
@@ -35,7 +37,13 @@ var noExpereience: [String] = []
 
 // logic that sorts all players into two groups, those with expereicnce and those that have never played before
 
-
+if players["Experience"] === "Yes" {
+    experience.append[players]
+} else if players["Experience"] == "No" {
+    noExpereince.append[players]
+}
+experience
+noExpereience
 
 //Create logic that can iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
 
