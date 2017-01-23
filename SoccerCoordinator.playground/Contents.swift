4c5c8e7c491292566d409e9be323ed5c6e86c139
Jparks <boatcoding@gmail.com>
2017-01-23 13:44:15 -0800

    Part 2 Issues...

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -25,33 +25,21 @@ let hKrustofski: [String: Any] = ["Name": "Herschel Krustofski", "Height": "45",
 let players = [jSmith, jTanner, bBon, eGordon, mGill, kStein, sAdams, kSaygan, sGreenberg, sDali, jKavalier, bFinkelstein, dSoto, cAlaska, aWillis, pHelm, lClay, hKrustofski]
 
 // variables holding the players for each team
-var dragons: [String: Any] = [:]
-var sharks: [String: Any] = [:]
-var raptors: [String: Any] = [:]
+var dragons = [Any]()
+var sharks = [Any]()
+var raptors = [Any]()
 
 // variables holding the players who have expereince with soccer vs those that do not
-var experience = 0
-var noExpereience = 0
+var experience = [Any]()
+var noExpereience = [Any]()
 
 // logic that sorts all players into two groups, those with experience and those that have never played before
 
 for player in players {
-    if player["Experience"] as! Bool == false {
-        experience.insert(player)
-} else {
+    if player["Experience"] as! Bool == true {
+       experience.append(player)
+    } else {
         noExpereience.append(player)
+    }
 }
-experience.count
-noExpereience.count
-
-
-
-
-
-
-
-
-
-
-
 
