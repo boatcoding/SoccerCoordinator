d7843af5a7b8781159f675757066d846e9553a6b
Jparks <boatcoding@gmail.com>
2017-01-24 11:19:00 -0800

    Part 2 - syntax update

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,24 +2,24 @@
 typealias Player = [String: Any]
 typealias PlayerArray = [Player]
 
-let player1: Player = ["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"]
-let player2: Player = ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"]
-let player3: Player = ["Name": "Bill Bonn", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"]
-let player4: Player = ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"]
-let player5: Player = ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"]
-let player6: Player = ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"]
-let player7: Player = ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"]
-let player8: Player = ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"]
-let player9: Player = ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"]
-let player10: Player = ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"]
-let player11: Player = ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"]
-let player12: Player = ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"]
-let player13: Player = ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"]
-let player14: Player = ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"]
-let player15: Player = ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"]
-let player16: Player = ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"]
-let player17: Player = ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"]
-let player18: Player = ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"]
+let player1: Player = ["Name": "Joe Smith", "Height": 42, "isExperienced": true, "Guardian": "Jim and Jan Smith"]
+let player2: Player = ["Name": "Jill Tanner", "Height": 36, "isExperienced": true, "Guardian": "Clara Tanner"]
+let player3: Player = ["Name": "Bill Bonn", "Height": 43, "isExperienced": true, "Guardian": "Sara and Jenny Bon"]
+let player4: Player = ["Name": "Eva Gordon", "Height": 45, "isExperienced": false, "Guardian": "Wendy and Mike Gordon"]
+let player5: Player = ["Name": "Matt Gill", "Height": 40, "isExperienced": false, "Guardian": "Charles and Sylvia Gill"]
+let player6: Player = ["Name": "Kimmy Stein", "Height": 41, "isExperienced": false, "Guardian": "Bill and Hillary Stein"]
+let player7: Player = ["Name": "Sammy Adams", "Height": 45, "isExperienced": false, "Guardian": "Jeff Adams"]
+let player8: Player = ["Name": "Karl Saygan", "Height": 42, "isExperienced": true, "Guardian": "Heather Bledsoe"]
+let player9: Player = ["Name": "Suzane Greenberg", "Height": 44, "isExperienced": true, "Guardian": "Henrietta Dumas"]
+let player10: Player = ["Name": "Sal Dali", "Height": 41, "isExperienced": false, "Guardian": "Gala Dali"]
+let player11: Player = ["Name": "Joe Kavalier", "Height": 39, "isExperienced": false, "Guardian": "Sam and Elaine Kavalier"]
+let player12: Player = ["Name": "Ben Finkelstein", "Height": 44, "isExperienced": false, "Guardian": "Aaron and Jill Finkelstein"]
+let player13: Player = ["Name": "Diego Soto", "Height": 41, "isExperienced": true, "Guardian": "Robin and Sarika Soto"]
+let player14: Player = ["Name": "Chloe Alaska", "Height": 47, "isExperienced": false, "Guardian": "David and Jamie Alaska"]
+let player15: Player = ["Name": "Arnold Willis", "Height": 43, "isExperienced": false, "Guardian": "Claire Willis"]
+let player16: Player = ["Name": "Phillip Helm", "Height": 44, "isExperienced": true, "Guardian": "Thomas Helm and Eva Jones"]
+let player17: Player = ["Name": "Les Clay", "Height": 42, "isExperienced": true, "Guardian": "Wynonna Brown"]
+let player18: Player = ["Name": "Herschel Krustofski", "Height": 45, "isExperienced": true, "Guardian": "Hyman and Rachel Krustofski"]
 
 let players: PlayerArray = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
 
@@ -34,7 +34,7 @@ var inexperiencedPlayers = [[String: Any]]()
 
 // logic that sorts all players into two groups, those with experience and the unexperienced
 for player in players {
-    if player["Experience"] as! Bool == true {
+    if player["isExperienced"] as! Bool == true {
         experiencedPlayers.append(player)
     } else {
         inexperiencedPlayers.append(player)
@@ -58,7 +58,3 @@ while counter > 0 {
     raptors.append(inexperiencedPlayers.removeFirst())
     counter -= 3
 }
-
-
-
-
