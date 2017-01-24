264388d2327be40a86fd5c84961d0fc3f8f58da2
Jparks <boatcoding@gmail.com>
2017-01-24 11:14:57 -0800

    Part 1 - updated collections via type alias Part 2 - updated using has- and is- for bool statements (used experiencedPlayers vs has/is)

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,82 +1,63 @@
-import UIKit
+// individual player constants are stored into their own dictionary as well as one constant that holds all the players, both of these groups are referenced to a typealias that holds the Type, which is 'String: Any'.
+typealias Player = [String: Any]
+typealias PlayerArray = [Player]
 
-// individual player variables are stored into their own dictionary
-let jSmith: [String: Any] = ["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"]
-let jTanner: [String: Any] = ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"]
-let bBon: [String: Any] = ["Name": "Bill Bonn", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"]
-let eGordon: [String: Any] = ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"]
-let mGill: [String: Any] = ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"]
-let kStein: [String: Any] = ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"]
-let sAdams: [String: Any] = ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"]
-let kSaygan: [String: Any] = ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"]
-let sGreenberg: [String: Any] = ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"]
-let sDali: [String: Any] = ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"]
-let jKavalier: [String: Any] = ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"]
-let bFinkelstein: [String: Any] = ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"]
-let dSoto: [String: Any] = ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"]
-let cAlaska: [String: Any] = ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"]
-let aWillis: [String: Any] = ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"]
-let pHelm: [String: Any] = ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"]
-let lClay: [String: Any] = ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"]
-let hKrustofski: [String: Any] = ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"]
+let player1: Player = ["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"]
+let player2: Player = ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"]
+let player3: Player = ["Name": "Bill Bonn", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"]
+let player4: Player = ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"]
+let player5: Player = ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"]
+let player6: Player = ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"]
+let player7: Player = ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"]
+let player8: Player = ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"]
+let player9: Player = ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"]
+let player10: Player = ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"]
+let player11: Player = ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"]
+let player12: Player = ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"]
+let player13: Player = ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"]
+let player14: Player = ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"]
+let player15: Player = ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"]
+let player16: Player = ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"]
+let player17: Player = ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"]
+let player18: Player = ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"]
 
-// all players within the league are now stored within this one array
-let players = [jSmith, jTanner, bBon, eGordon, mGill, kStein, sAdams, kSaygan, sGreenberg, sDali, jKavalier, bFinkelstein, dSoto, cAlaska, aWillis, pHelm, lClay, hKrustofski]
+let players: PlayerArray = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
 
 // variables holding the players for each team
-var dragons = [Any]()
-var sharks = [Any]()
-var raptors = [Any]()
+var dragons = [[String: Any]]()
+var sharks = [[String: Any]]()
+var raptors = [[String: Any]]()
 
 // variables holding the players who have expereince with soccer vs those that do not
-var experience = [Any]()
-var noExperience = [Any]()
+var experiencedPlayers = [[String: Any]]()
+var inexperiencedPlayers = [[String: Any]]()
 
 // logic that sorts all players into two groups, those with experience and the unexperienced
 for player in players {
     if player["Experience"] as! Bool == true {
-       experience.append(player)
+        experiencedPlayers.append(player)
     } else {
-        noExperience.append(player)
+        inexperiencedPlayers.append(player)
     }
 }
 
 //logic that sorts experienced players into three even teams
-var count = experience.count
+var count = experiencedPlayers.count
 while count > 0 {
-    dragons.append(experience.removeFirst())
-    sharks.append(experience.removeFirst())
-    raptors.append(experience.removeFirst())
+    dragons.append(experiencedPlayers.removeFirst())
+    sharks.append(experiencedPlayers.removeFirst())
+    raptors.append(experiencedPlayers.removeFirst())
     count -= 3
 }
 
 //logic that sorts unexperienced players into three even teams
-var counter = noExperience.count
+var counter = inexperiencedPlayers.count
 while counter > 0 {
-    dragons.append(noExperience.removeFirst())
-    sharks.append(noExperience.removeFirst())
-    raptors.append(noExperience.removeFirst())
+    dragons.append(inexperiencedPlayers.removeFirst())
+    sharks.append(inexperiencedPlayers.removeFirst())
+    raptors.append(inexperiencedPlayers.removeFirst())
     counter -= 3
 }
-
-// logic that adds the height of all the players on each team
-if dragonsHeight dragons["Height"] as! [Int] = {
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
