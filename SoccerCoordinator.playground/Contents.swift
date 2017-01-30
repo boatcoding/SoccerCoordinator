ee8bb9fd811a32953a2c5ffd67e8825edfeff9bc
Jparks <boatcoding@gmail.com>
2017-01-30 13:31:28 -0800

    Part 3 - function issues

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -153,6 +153,7 @@ func guardianLetterBy(teamName: [[String:Any]]) -> String {
     let player = guardianInfo["Name"] as! String
     let team = "TEAMNAME"
     let practice = "TEAMPRACTICE"
+    // if guardianInfo is a collection of dragons {then assign to dragonsName && dragonsPractice} hmmmm.
     let messageForGuardian = "Hi \(guardian), \(player) has been officially accepted to the \(team)!!! The first team pracice will be held on \(practice) at St. Johns Cathedral park in Portland. We look forward to having \(player) on the team! Thanks, The little league soccer volunteers"
   }
 } return messageForGuardian
